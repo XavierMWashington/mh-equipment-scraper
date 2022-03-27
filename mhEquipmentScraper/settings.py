227d0bd0fa2654af3cef4a16728b19c2a07d0c0d
unknown <RashaanWashington@protonmail.com>
2022-03-27 13:03:54 -0400

    static file handling

--- mhEquipmentScraper/settings.py
+++ mhEquipmentScraper/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://mh-equip-api.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['mh-equip-api.herokuapp.com', 'localhost']
 
 
 # Application definition
